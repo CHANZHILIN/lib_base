6718e3421f3b233bc455d6b46caa7472fc17d125
chanzhilin <1181785848@qq.com>
2019-03-27 12:48:14 +0800

    first commit

---
+++ src/main/java/chen/baselib/Constants.java
@@ -0,0 +1,10 @@
+package chen.baselib;
+
+/**
+ * Introduce :  ARouter跳转的链接
+ * Created by CHEN_ on 2019/3/26.
+ * version:2.0
+ **/
+public interface Constants {
+    String LOGIN_PATH = "/module_login/loginActivity";
+}
